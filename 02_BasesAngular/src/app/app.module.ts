825f88fef02efc2141e1f94171f3c7cf9e4a7682
Javier Flores <jffc.dev@gmail.com>
2022-10-16 02:37:23 -0500

    feat(app): Counter module created

--- 02_BasesAngular/src/app/app.module.ts
+++ 02_BasesAngular/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+import { CounterModule } from './counter/Counter.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
