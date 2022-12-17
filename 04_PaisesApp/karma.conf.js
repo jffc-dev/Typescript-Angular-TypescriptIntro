22b9945d19d25133839d338bb473698e9dbf42d2
Javier Flores <jffc.dev@gmail.com>
2022-12-16 23:53:08 -0500

    info(countryApp): Country App info changed

--- 04_PaisesApp/karma.conf.js
+++ 04_PaisesApp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/paises-app'),
+      dir: require('path').join(__dirname, './coverage/countries-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
