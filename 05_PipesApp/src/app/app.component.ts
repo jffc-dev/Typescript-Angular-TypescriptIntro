141504e36733aef74ffb82acb28ee50686840810
Javier Flores <jffc.dev@gmail.com>
2022-12-24 01:31:47 -0500

    feat(PipesApp): Pipes introduction

--- 05_PipesApp/src/app/app.component.ts
+++ 05_PipesApp/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  name: string = 'jaVier fLores'
+  value: number = 1000
+  obj = {
+    name: 'Javier'
+  }
+
+  showName(){
+    console.log(this.name);
+    console.log(this.value);
+    console.log(this.obj);
+  }
 }
