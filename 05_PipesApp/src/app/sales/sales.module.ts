4466386c6ae502c204f749cdec4344bb777e1185
Javier Flores <jffc.dev@gmail.com>
2023-01-05 00:03:13 -0500

    feat(PipesApp): Router added

--- 05_PipesApp/src/app/sales/sales.module.ts
+++ 05_PipesApp/src/app/sales/sales.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NumbersComponent } from './pages/numbers/numbers.component';
+import { NoCommonsComponent } from './pages/no-commons/no-commons.component';
+import { BasicsComponent } from './pages/basics/basics.component';
+import { SortComponent } from './pages/sort/sort.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NumbersComponent,
+    NoCommonsComponent,
+    BasicsComponent,
+    SortComponent
+  ],
   imports: [
     CommonModule
   ]
