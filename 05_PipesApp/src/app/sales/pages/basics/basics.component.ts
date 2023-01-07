5172e311a4825e3e65a16f4cf3e779dd542111d3
Javier Flores <jffc.dev@gmail.com>
2023-01-06 23:38:14 -0500

    feat(PipesApp): Lowercase, uppercase and titlecase pipes examples added

--- 05_PipesApp/src/app/sales/pages/basics/basics.component.ts
+++ 05_PipesApp/src/app/sales/pages/basics/basics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basics',
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicsComponent implements OnInit {
+export class BasicsComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  nameLower: string = "javier";
+  nameUpper: string = "FLORES";
+  nameFull: string = "JaVIER FlorES";
 
 }
