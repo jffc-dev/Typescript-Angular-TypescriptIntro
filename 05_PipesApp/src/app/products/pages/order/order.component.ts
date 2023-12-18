b985b1b4927dd4b3d59fd4130f52e53c570daecd
Javier Flores <jffc.dev@gmail.com>
2023-12-18 01:37:38 -0500

    feat: toggleUpperCase function added

--- 05_PipesApp/src/app/products/pages/order/order.component.ts
+++ 05_PipesApp/src/app/products/pages/order/order.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-order',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent {
 
-  constructor() { }
+  public isUpperCase: boolean = false;
 
-  ngOnInit(): void {
+  toggleUpperCase(): void{
+    this.isUpperCase = !this.isUpperCase
   }
 
 }
