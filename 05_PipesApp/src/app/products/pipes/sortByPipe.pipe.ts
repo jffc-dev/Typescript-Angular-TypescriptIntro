419ca84f0ab2782cee942f2e6f8375fbb885a010
Javier Flores <jffc.dev@gmail.com>
2023-12-22 23:48:16 -0500

    feat: SortByPipe sort improved

--- 05_PipesApp/src/app/products/pipes/sortByPipe.pipe.ts
+++ 05_PipesApp/src/app/products/pipes/sortByPipe.pipe.ts
@@ -8,16 +8,11 @@ export class SortByPipe implements PipeTransform{
 
   transform(heroes: Hero[], sortBy?: keyof Hero | ''): Hero[] {
 
-    switch(sortBy){
-      case 'name':
-        return heroes.sort((a,b)=> (a.name > b.name) ? 1 : -1)
-      case 'canFly':
-        return heroes.sort((a,b)=> (a.canFly > b.canFly) ? 1 : -1)
-      case 'color':
-        return heroes.sort((a,b)=> (a.color > b.color) ? 1 : -1)
-      default:
-        return heroes
-    }
+    if(sortBy)
+      return heroes.sort((a,b)=> (a[sortBy] > b[sortBy]) ? 1 : -1)
+    else
+      return heroes
+
   }
 
 }
