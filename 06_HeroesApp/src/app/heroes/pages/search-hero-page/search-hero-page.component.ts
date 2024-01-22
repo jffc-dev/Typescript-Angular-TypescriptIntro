512d1df8f9f5f91b0e511a2dbd05bb1d49fb2174
Javier Flores <jffc.dev@gmail.com>
2024-01-21 23:10:13 -0500

    feat: searchHero and mat-autocomplete added

--- 06_HeroesApp/src/app/heroes/pages/search-hero-page/search-hero-page.component.ts
+++ 06_HeroesApp/src/app/heroes/pages/search-hero-page/search-hero-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-search-hero-page',
@@ -13,4 +14,17 @@ export class SearchHeroPageComponent {
   constructor(private heroesService: HeroesService){}
 
   public searchInput = new FormControl('')
+  public heroes: Hero[] = []
+
+  searchHero(){
+    const value: string = this.searchInput.value || '';
+
+    this.heroesService.getSuggestions(value).subscribe(
+
+
+      heroes => {this.heroes = heroes
+        console.log(heroes);}
+    )
+
+  }
 }
